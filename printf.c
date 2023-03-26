86071e22d547b4563426b36ab9ab7d1e7a9b2764
OmarSalah1911 <omar.salah1597@gmail.com>
2023-03-27 01:31:44 +0200

    betty

--- printf.c
+++ printf.c
@@ -4,34 +4,42 @@
 
 int _printf(const char *format, ...)
 {
-    va_list args;
-    va_start(args, format);
-    char c, *s;
+va_list args;
+va_start(args, format);
+char c, *s;
 
-    int num_chars_printed = 0;
-    while (*format != '\0') {
-        if (*format == '%') {
-            format++;
-            if (*format == 'c') {
-                c = va_arg(args, int);
-                _putchar(c);
-                num_chars_printed++;
-            } else if (*format == 's') {
-                s = va_arg(args, char *);
-                fputs(s, stdout);
-                num_chars_printed += strlen(s);
-            } else if (*format == '%') {
-                _putchar('%');
-                num_chars_printed++;
-            }
-        } else {
-            _putchar(*format);
-            num_chars_printed++;
-        }
-        format++;
-    }
-    
-    va_end(args);
-    
-    return num_chars_printed;
+unsigned int num_chars_printed = 0;
+while (*format != '\0')
+{
+if (*format == '%')
+{
+format++;
+if (*format == 'c')
+{
+c = va_arg(args, int);
+_putchar(c);
+num_chars_printed++;
+} else if (*format == 's')
+{
+*s = va_arg(args, char *);
+fputs(s, stdout);
+num_chars_printed += strlen(s);
+}
+else if (*format == '%')
+{
+_putchar('%');
+num_chars_printed++;
+}
+}
+else
+{
+_putchar(*format);
+num_chars_printed++;
+}
+format++;
+}
+
+va_end(args);
+
+return num_chars_printed;
 }
