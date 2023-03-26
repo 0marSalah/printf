7270b4fb2e941a939e7d3ac97955ec8a2389a845
OmarSalah1911 <omar.salah1597@gmail.com>
2023-03-27 01:23:42 +0200

    testing

--- printf.c
+++ printf.c
@@ -1,37 +1,49 @@
 #include <stdarg.h>
-#include <string.h>
+#include <stdio.h>
 #include "main.h"
 
 int _printf(const char *format, ...)
 {
+    int count = 0;
     va_list args;
     va_start(args, format);
-    
-    int num_chars_printed = 0;
-    printf("%d", 5)
+
     while (*format != '\0') {
         if (*format == '%') {
             format++;
-            if (*format == 'c') {
-                char c = va_arg(args, int);
-                _putchar(c);
-                num_chars_printed++;
-            } else if (*format == 's') {
-                char *s = va_arg(args, char *);
-                fputs(s, stdout);
-                num_chars_printed += strlen(s);
-            } else if (*format == '%') {
-                _putchar('%');
-                num_chars_printed++;
+            switch (*format) {
+                case 'c': {
+                    char c = (char)va_arg(args, int);
+                    putchar(c);
+                    count++;
+                    break;
+                }
+                case 's': {
+                    char *s = va_arg(args, char *);
+                    while (*s != '\0') {
+                        putchar(*s);
+                        s++;
+                        count++;
+                    }
+                    break;
+                }
+                case '%': {
+                    putchar('%');
+                    count++;
+                    break;
+                }
+                default: {
+                    // Invalid conversion specifier
+                    return -1;
+                }
             }
         } else {
-            _putchar(*format);
-            num_chars_printed++;
+            putchar(*format);
+            count++;
         }
         format++;
     }
-    
+
     va_end(args);
-    
-    return num_chars_printed;
+    return count;
 }
